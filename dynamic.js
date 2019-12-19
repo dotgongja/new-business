1bb00bb7e8811b59cf146e7e8bb7879557c12538
dotgongja <sung.chang12@gmail.com>
2019-12-19 10:19:26 -0800

    create js script for if/else statement

---
+++ dynamic.js
@@ -0,0 +1,9 @@
+var bike = prompt("Do you like riding bikes Yes/No?");
+
+if (bike === "yes") {
+    alert("Great, so do we!");
+}else if (bike === "no") {
+    alert("No, problem we didnt at first either!");
+}else{
+    alert("Ok, come back again to check us out!");
+}
